7418f725e671abd02cc0a611b029f4d37ea139c7
Hyeonproject <axzswq@gmail.com>
2020-12-09 17:24:12 +0900

    update pagination

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -165,7 +165,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
 
